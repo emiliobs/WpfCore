656e2df1a991479b8e9d9f5cd9c6216824385186
Emilio Barrea <barrera_emilio@yahoo.es>
2020-07-03 01:38:53 +0100

    Add project files.

---
+++ WpfCore/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WpfCore.Models;
+
+namespace WpfCore.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        //Databse Configure:
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //Connection String:
+            optionsBuilder.UseSqlServer(@"Data Source=.;Initial Catalog=StudentWpf;Integrated Security=True;Pooling=False");
+        }
+    }
+}
---
+++ WpfCore/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System.Linq;
+using System.Windows;
+using WpfCore.Data;
+using WpfCore.Models;
+
+namespace WpfCore
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //using (var db = new AppDbContext())
+            //{
+            //    var studentData = db.Students.ToList();
+            //    StudentList.Items.Add(studentData);
+            //}
+
+            PopulateStudentData();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var studentName = strName.Text;
+            var studentAddress = strAddress.Text;
+
+            using (var db = new AppDbContext())
+            {
+                db.Students.Add(new Student
+                {
+                    Name = studentName,
+                    Address = studentAddress,
+                });
+
+                //var student = new Student 
+                //{
+                //    Name = studentName,
+                //    Address = studentAddress,
+                //};
+
+                //db.Students.Add(student);
+                db.SaveChanges();
+                StudentList.Items.Clear();
+                PopulateStudentData();
+            }
+        }
+
+        private void PopulateStudentData()
+        {
+            using (var db = new AppDbContext())
+            {
+                var studentData = db.Students.ToList();
+                foreach (var student in studentData)
+                {
+                    StudentList.Items.Add(student);
+
+                }
+            }
+        }
+    }
+}
